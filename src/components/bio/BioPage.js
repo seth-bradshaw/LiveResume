854ef89c416948aa27c3f97aa05d10a9f6c763b2
seth-bradshaw <seth.bradshaw27@gmail.com>
2021-07-27 13:16:38 -0700

    fixed: footer not displaying icons, filled in text

--- src/components/bio/BioPage.js
+++ src/components/bio/BioPage.js
@@ -12,44 +12,51 @@ export default function BioPage() {
         <span className="mt-0">
           <img
             className="rounded-full w-48 h-48 lg:w-56 lg:h-56 z-0"
-            src="Profile_Pic.jpg"
+            src="Profile_Pic.jpeg"
             alt="Seth Bradshaw"
           />
         </span>
         <div className="mt-10 flex flex-col items-center text-center p-5 sm:p-8">
           <p className="text-white text-lg mb-10 w-full lg:w-2/3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            egestas, est sed convallis lacinia, tortor mi sagittis nulla, et
-            sagittis purus quam tincidunt odio. Donec vitae sodales arcu. Donec
-            in ex quis magna efficitur condimentum vel eu magna. Donec eu ipsum
-            nibh. Donec augue massa, porttitor sit amet dolor sit amet, tempor
-            interdum nunc. Quisque scelerisque consequat ante, sagittis
-            malesuada lectus facilisis id. Donec eget dolor diam. Praesent nec
-            condimentum lectus. Nam gravida felis id ex accumsan, eget venenatis
-            tortor sodales. Nullam vestibulum mauris lectus. Mauris et purus at
-            metus dapibus cursus id quis ex. Vivamus vitae augue vel libero
-            congue cursus. In cursus libero eu elit mattis scelerisque.
+            I am a compassionate, caring person who loves to make others happy
+            and improve their lives. There is a very big soft spot in my heart
+            for all animals, especially dogs. I truly believe humans have a lot
+            to learn from them. I am an avid sports fan of the Utah Jazz and
+            Philadelphia Eagles, and enjoyed participating in sports throughout
+            my life. I’ve been drawn to technology and science from a very young
+            age, which ultimately led to me finding a career in those fields. I
+            am so fortunate to have found software engineering, I’ve found it to
+            be the perfect medium for me to exercise my creative problem solving
+            skills, and explore new challenging ideas, while building products
+            that make a positive impact in others lives.
           </p>
           <p className="text-white text-lg mb-10 w-full lg:w-2/3">
-            Etiam mauris nulla, interdum vel lorem a, finibus imperdiet dui.
-            Nulla bibendum est nisi, id condimentum arcu ultrices ac. Mauris sed
-            condimentum libero. Suspendisse eget lectus fringilla, molestie
-            massa vel, porttitor est. Aenean lobortis varius quam, eget lobortis
-            felis dictum sed. Etiam et augue vel dolor vestibulum rutrum. Nulla
-            eu viverra magna, ut porttitor felis. Vivamus maximus dui aliquet
-            efficitur convallis. Vivamus id bibendum orci, nec pretium ante.
+            In the summer of 2020, I was working from home due to a global
+            pandemic as a Customer Service Representative for Bed Bath and
+            Beyond. One day while my computer was booting up and I’m preparing
+            for the start of my shift, I thought to myself “I’m going to be
+            doing this for another 4 years”. The thought made my skin crawl. The
+            plan at the time was to get a four year degree in Computer Science,
+            then begin my career. Once my computer booted up, I checked my email
+            and was informed that the entire customer service team will be
+            outsourced in 2 months. After the initial shock settled in, I
+            decided then and there that in 2 months I was concluding my final
+            job of not being a software engineer. After much thought, I found
+            Lambda school, and decided that it would be the fastest means to get
+            a quality education and find a job as a Software Engineer. I loved
+            my time at Lambda, I found some great friends, learned vital
+            technologies modern Web Developers need under their belt, and more
+            importantly I discovered how much I love learning, and it opened up
+            a thirst for knowledge that will never be quenched.
           </p>
           <p className="text-white text-lg w-full lg:w-2/3">
-            Aenean semper, turpis a aliquet ultricies, augue enim fermentum
-            eros, ut euismod quam libero nec libero. Mauris laoreet lorem et
-            urna elementum consequat. Pellentesque elementum, elit vitae
-            placerat rutrum, ante mauris cursus nunc, sit amet ornare leo ipsum
-            nec metus. Aenean gravida, quam quis rutrum mollis, sem dui
-            malesuada ex, id sollicitudin ex dui nec mauris. In at cursus odio,
-            ut scelerisque massa. Nullam vitae est condimentum, ullamcorper sem
-            eu, egestas mauris. Nunc vitae venenatis nunc. In hac habitasse
-            platea dictumst. Duis pretium, massa at accumsan ullamcorper, mi
-            massa placerat risus, vel porttitor massa neque et ligula.
+            As far as my ultimate career goals, I plan to pursue a double major
+            in Physics and Computer Science at ASU. The climate crisis is the
+            biggest threat to life that humans have seen, so as someone who
+            cherishes all life I want to work with like minded individuals in
+            green energy, or astrophysics to help resolve these issues. Although
+            the future may seem grim, I have taken it a personal responsibility
+            to help make this world a better home for generations to come.
           </p>
         </div>
       </div>
