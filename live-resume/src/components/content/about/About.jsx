7f605b173053cd91298543614eea374a3382b411
seth-bradshaw <seth.bradshaw27@gmail.com>
2024-02-20 14:37:06 -0700

    making eslint happy

--- live-resume/src/components/content/about/About.jsx
+++ live-resume/src/components/content/about/About.jsx
@@ -5,27 +5,27 @@ export default function About() {
   return (
     <Section id="about" title="about">
       In 2019, faced with the prospect of my customer service job being
-      outsourced, I made a bold decision: I wouldn't settle for anything less
+      outsourced, I made a bold decision: I wouldn&apos;t settle for anything less
       than becoming a software engineer. Over the next year, I dedicated myself
       to mastering the craft, culminating in an opportunity to contribute to
-      Carvana's search page—an experience that shaped me both technically and
+      Carvana&apos;s search page—an experience that shaped me both technically and
       professionally.
       <br></br>
       <br></br>
       Although my time at Carvana was cut short by a round of layoffs, it
       provided invaluable lessons and propelled me forward. Since then, I
-      opportunity to revamp Collision and Forensics Engineering Inc's website,
+      opportunity to revamp Collision and Forensics Engineering Inc&apos;s website,
       enhancing the user experience and leaving a lasting impact.
       <br></br>
       <br></br>
-      Now, I'm on the lookout for my next challenge—a company where I can
+      Now, I&apos;m on the lookout for my next challenge—a company where I can
       continue my journey as a software engineer and thrive both personally and
       professionally. With a relentless curiosity and a passion for solving
-      complex problems, I'm ready to make a meaningful contribution wherever I
+      complex problems, I&apos;m ready to make a meaningful contribution wherever I
       go.
       <br></br>
       <br></br>
-      Outside of coding, you'll find me spending time with my wife and dog,
+      Outside of coding, you&apos;ll find me spending time with my wife and dog,
       pushing my limits at the gym, finding peace through meditation, and
       playing video games.
     </Section>
